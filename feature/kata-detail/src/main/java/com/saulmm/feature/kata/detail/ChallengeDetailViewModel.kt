7f1530d1e9860e7c832cc0c12af02f2cdd1d7378
saulmm <saulmm2@gmail.com>
2023-08-27 22:36:33 +0200

    Created ChallengeDetailViewModel

---
+++ feature/kata-detail/src/main/java/com/saulmm/feature/kata/detail/ChallengeDetailViewModel.kt
@@ -0,0 +1,29 @@
+package com.saulmm.feature.kata.detail
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+
+class ChallengeDetailViewModel @AssistedInject constructor(
+    @Assisted private val challengeId: String
+) {
+
+    @AssistedFactory
+    interface Factory {
+        fun create(challengeId: String): ChallengeDetailViewModel
+    }
+
+    companion object {
+        fun provideFactory(
+            assistedFactory: Factory,
+            challengeId: String
+        ): ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return assistedFactory.create(challengeId) as T
+            }
+
+        }
+    }
+}
