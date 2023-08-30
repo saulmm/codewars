4aab8c6649e96fe95d9d241ff1694d1e2b84561f
saulmm <saulmm2@gmail.com>
2023-08-30 16:43:20 +0200

    Revert "Solved compilation warning"
    This reverts commit 2561171d710f72d84a5a5382bb666567bcffa8f8.

--- buildSrc/src/main/kotlin/com/saulmm/codewars/buildsrc/BuildConstants.kt
+++ buildSrc/src/main/kotlin/com/saulmm/codewars/buildsrc/BuildConstants.kt
@@ -5,7 +5,7 @@ import org.gradle.api.JavaVersion
 object BuildConstants {
     const val MIN_SDK = 24
     const val TARGET_SDK = 33
-    const val COMPILE_SDK = 33
+    const val COMPILE_SDK = 34
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0"
     val JAVA_VERSION = JavaVersion.VERSION_17
